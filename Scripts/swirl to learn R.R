38291cb571a9d3666baa6908c5ba971e0a271318
AhsanHimel <ahsanulislam10@gmail.com>
2020-10-09 15:35:16 +0600

    init

---
+++ Scripts/swirl to learn R.R
@@ -0,0 +1,22 @@
+# Since swirl is an R package, you can easily install it by 
+# entering a single command from the R console
+install.packages("swirl")
+
+# Load swirl
+library(swirl)
+
+# Install the R Progroamming course
+install_from_swirl("R Programming")
+
+# Start swirl and complete the lessons
+swirl()
+
+# | When you are at the R prompt (>):
+# | -- Typing skip() allows you to skip the current question.
+# | -- Typing play() lets you experiment with R on your own; swirl will ignore what you do...
+# | -- UNTIL you type nxt() which will regain swirl\'s attention.
+# | -- Typing bye() causes swirl to exit. Your progress will be saved.
+# | -- Typing main() returns you to swirl\'s main menu.
+# | -- Typing info() displays these options again.
+
+
