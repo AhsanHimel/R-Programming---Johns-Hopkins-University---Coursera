247c1011dd94fda22366abd10a9c204754fbda61
AhsanHimel <ahsanulislam10@gmail.com>
2020-10-09 17:36:17 +0600

    Completed function

--- Scripts/Programming Assignment 2 - Lexical Scoping.R
+++ Scripts/Programming Assignment 2 - Lexical Scoping.R
@@ -12,7 +12,7 @@ makeCacheMatrix <- function(x = matrix()) {
   return(list(set = set,
               get = get,
               setInverse = setInverse,
-              getInverse = getInverse) )
+              getInverse = getInverse))
 }
 
 makeCacheMatrix <- function(x = matrix()) {
@@ -52,17 +52,39 @@ cacheSolve <- function(x, ...) {
 source("./Scripts/Programming Assignment 2 - Lexical Scoping.R")
 
 mat <- makeCacheMatrix(matrix(c(23,34,12,
-                                      56,78,32,
-                                      0,2,40), 
-                              nrow = 3, ncol = 3, byrow=T))
-
+                                56,78,32,
+                                0,2,40), 
+                                nrow = 3, ncol = 3, byrow=T))
 mat$get()   # to see the created matrix
+# [,1] [,2] [,3]
+# [1,]   23   34   12
+# [2,]   56   78   32
+# [3,]    0    2   40
 mat$getInverse()    # to see the inverse matrix if exists
+# NULL
 cacheSolve(mat)   # inverses the matrix
+# [,1]        [,2]        [,3]
+# [1,] -0.67491166  0.29505300 -0.03356890
+# [2,]  0.49469965 -0.20318021  0.01413428
+# [3,] -0.02473498  0.01015901  0.02429329
 mat$getInverse()    # now the inverse matrix will be shown
+# [,1]        [,2]        [,3]
+# [1,] -0.67491166  0.29505300 -0.03356890
+# [2,]  0.49469965 -0.20318021  0.01413428
+# [3,] -0.02473498  0.01015901  0.02429329
 mat$set(matrix(c(20, 12, 41,94), 2, 2))   # set a new matrix
 mat$get()   # see the new matrix
+# [,1] [,2]
+# [1,]   20   41
+# [2,]   12   94
 mat$getInverse()
+# NULL
 cacheSolve(mat)
+# [,1]        [,2]
+# [1,]  0.067723343 -0.02953890
+# [2,] -0.008645533  0.01440922
 mat$getInverse()
+# [,1]        [,2]
+# [1,]  0.067723343 -0.02953890
+# [2,] -0.008645533  0.01440922
 
